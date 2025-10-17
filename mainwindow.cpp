0b1499579fc28654b61a10b3701d0c4d439d7e02
LiuWeihua <3104455288@qq.com>
2025-10-17 08:39:50 +0800

    first version

---
+++ mainwindow.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->btnNum0,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum1,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum2,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum3,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum4,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum5,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum6,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum7,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum8,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum9,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::btnNumClicked()
+{
+    ui->statusbar->showMessage("btn clicked");
+}
